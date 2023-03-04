654e7988d105323c4d4f5a0fce68b342b21541e3
BANTaeHee <ban7598@gmail.com>
2023-03-04 17:22:09 +0900

    2023-03-04 SpringBoot 실습(N:1 다대일관계) - 게시물 등록

---
+++ src/main/java/com/example/demo/service/BoardServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.BoardDTO;
+import com.example.demo.entity.Board;
+import com.example.demo.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class BoardServiceImpl implements BoardService{
+    private final BoardRepository repository;   // 자동 주입 final
+    @Override
+    public Long register(BoardDTO dto) {
+
+        log.info(dto);
+
+        Board board = dtoToEntity(dto);
+
+        repository.save(board);
+
+        return board.getBno();
+    }
+}
